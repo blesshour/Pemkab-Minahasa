878fc17cfd154c6ae8a46c60282fbde6e8d5a1f1
pricelangela <45728920+pricelangela@users.noreply.github.com>
2018-12-11 00:56:09 +0800

    banner.php

---
+++ banner.php
@@ -0,0 +1,40 @@
+<body>
+<link rel="stylesheet" href="inc/owl-carousel/owl.carousel.min.css">
+<link rel="stylesheet" href="inc/owl-carousel/owl.theme.default.min.css">
+ <!--  <section id="demos">
+    <div class="row">
+      <div class="large-12 columns">
+        <div class="owl-carousel owl-theme">
+          <div class="item">
+            <img src="img/caraousel(1).png" >
+          </div>
+          <div class="item">
+            <img src="img/caraousel(2).png" >
+          </div>
+          <div class="item">
+            <img src="img/caraousel(3).png" >
+          </div>
+        </div>
+      </div>
+    </div>
+  </section> -->
+
+  <div class="owl-carousel owl-theme">
+    <div class="item" style="width:250px"><h4>1</h4></div>
+    <div class="item" style="width:100px"><h4>2</h4></div>
+    <div class="item" style="width:500px"><h4>3</h4></div>
+    <div class="item" style="width:100px"><h4>4</h4></div>
+    <div class="item" style="width:50px"><h4>6</h4></div>
+    <div class="item" style="width:250px"><h4>7</h4></div>
+    <div class="item" style="width:120px"><h4>8</h4></div>
+    <div class="item" style="width:420px"><h4>9</h4></div>
+    <div class="item" style="width:120px"><h4>10</h4></div>
+    <div class="item" style="width:300px"><h4>11</h4></div>
+    <div class="item" style="width:450px"><h4>12</h4></div>
+    <div class="item" style="width:220px"><h4>13</h4></div>
+    <div class="item" style="width:150px"><h4>14</h4></div>
+    <div class="item" style="width:600px"><h4>15</h4></div>
+</div>
+
+<script src="js/main.js"></script>
+</body>
