506825bdf8940d00e77cb772e2a75d532230e5a3
BillyOroh <45728852+BillyOroh@users.noreply.github.com>
2018-12-11 03:32:15 +0800

    Add files via upload

---
+++ components-socmed.php
@@ -0,0 +1,25 @@
+<?php include 'header.php'; ?>
+<!-- SOCMED -->
+    <div class="row justify-content-center">
+        <div class="col-10 info-panel">
+          <div class="row">
+            <div class="col-lg">
+              <a href="#"><i class="fab fa-facebook fa-5x"></i></a>
+            </div>
+            <div class="col-lg">
+              <a href="#"><i class="fab fa-twitter-square fa-5x"></i></a>
+            </div>
+            <div class="col-lg">
+              <a href="#"><i class="fab fa-instagram fa-5x"></i></a>
+            </div>
+            <div class="col-lg">
+             <a href="#"><i class="fab fa-google-plus-g fa-5x"></i></a>
+            </div>
+            <div class="col-lg">
+              <a href="#"><i class="fab fa-youtube fa-5x"></i></a>
+            </div>
+          </div>
+        </div>
+      </div>
+    <!-- AKHIR SOCMED -->
+    <?php include 'footer.php'; ?>
