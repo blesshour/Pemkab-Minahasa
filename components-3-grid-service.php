1f4b728bf8c200f70ae9679c55ed16fb11235abd
BillyOroh <45728852+BillyOroh@users.noreply.github.com>
2018-12-11 00:55:59 +0800

    Add files via upload

---
+++ components-3-grid-service.php
@@ -0,0 +1,33 @@
+
+        
+     
+    <!-- 3 GRID -->
+    <section class="components-3-grid" id="components-3-grid">
+	    <div class="row justify-content-center">
+	        <div class="col-10 info-panel">
+	          <div class="row">
+	            <div class="col-lg text-center">
+	              <span style="color: Tomato;">
+	              <i class="fas fa-handshake fa-5x"></i>
+	              </span>
+	              <h4>Layanan Public</h4>
+	            </div>
+	            <div class="col-lg text-center">
+	              <span style="color: Dodgerblue;">
+	              <i class="fas fa-comments fa-5x"></i>
+	              </span>
+	              <h4>Suara Anda</h4>
+	            </div>
+	            <div class="col-lg text-center">
+	              <span style="color: Tomato;">
+	              <i class="fas fa-phone-square fa-5x"></i>
+	              </span>
+	              <h4>Nomor Telepon Penting</h4>
+	            </div>
+	          </div>
+	        </div>
+	      </div>
+      </section>
+      <!-- AKHIR 3 GRID -->                
+
+     
