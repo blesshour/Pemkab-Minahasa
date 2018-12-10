674b2c5e9a59827fdefd44c282011b66a7dac853
blesscious <45728403+blesscious@users.noreply.github.com>
2018-12-11 03:42:34 +0800

    Add array

--- components-si-terpadu.php
+++ components-si-terpadu.php
@@ -1,27 +1,51 @@
+<?php
+   //array sementara sebelum menggunakan database
+   //sistem informasi terpadu dengan data nama dan icon
+
+   $si = [
+      
+      [ 'nama' => 'PANGAN',
+        'icon' => 'warehouse' ],
+      
+      [ 'nama' => 'KEAMANAN',
+        'icon' => 'exclamation-circle' ],
+
+      [ 'nama' => 'KESEHATAN',
+        'icon' => 'medkit' ],
+
+      [ 'nama' => 'AMBULANCE',
+        'icon' => 'ambulance' ],
+
+   ]
+?>
+
 <?php include 'header.php'; ?>
    
    <section id="siterpadu">
-      <div class="container-fluid padding">
-         <div class="row">
-            <!--loop 4x tiap box-->
-            <?php for($x = 0; $x < 4; $x++): ?>
+      <div class="container-fluid padding"> <!--start container-->
+         <div class="row"> <!--row awal-->
+            
+            <!--memanggil setiap data di array sistem informasi ($si)-->
+            <?php foreach($si as $s): ?>
 
-               <div class="col-lg-3 col-md-6 col-sm-12 box fadeInRight">
-                  <div class="row in-box">
-                     <div class="icon col-2 text-center">
-                        <i class="fas fa-phone"></i>
-                     </div>
-                     <div class="caption col-10">
-                        <h5>Sistem Informasi</h5>
-                        <h4>(nama Sistem Informasi)</h4>
+               <div class="col-lg-3 col-md-6 col-sm-12 text-center">
+                  <div class="box fadeInRight">
+                     <div class="row in-box">
+                        <div class="icon col-3 text-center">
+                           <i class="fas fa-<?php echo $s['icon'];?>"></i>
+                        </div>
+                        <div class="caption col-9">
+                           <h4>Sistem Informasi</h5>
+                           <h2><?php echo $s['nama'];?></h4>
+                        </div>
                      </div>
                   </div>
                </div>
    
-            <?php endfor; ?>
+            <?php endforeach; ?>
 
-         </div>
-      </div>
+         </div> <!--end row awal-->
+      </div> <!--end container-->
    </section>
 
-<!--  <?php include 'footer.php'; ?> -->
+<?php include 'footer.php'; ?>
