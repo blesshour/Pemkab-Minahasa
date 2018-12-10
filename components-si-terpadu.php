f31d346b45210d20e1099f4e7f7b46a314e9de36
blesscious <45728403+blesscious@users.noreply.github.com>
2018-12-11 02:49:11 +0800

    Section SI Terpadu
    yang dpe file main.css, main.css.map, main.scss taruh di folder css

---
+++ components-si-terpadu.php
@@ -0,0 +1,35 @@
+<?php
+
+$x = 4;
+
+
+
+
+include 'header.php'; 
+?>
+   
+   <section id="siterpadu">
+      <div class="container-fluid padding">
+         <div class="row">
+
+            <?php for($x = 0; $x < 4; $x++): ?>
+
+               <div class="col-lg-3 col-md-6 col-sm-12 box fadeInRight">
+                  <div class="row in-box">
+                     <div class="icon col-2 text-center">
+                        <i class="fas fa-phone"></i>
+                     </div>
+                     <div class="caption col-10">
+                        <h5>Sistem Informasi</h5>
+                        <h4>(nama Sistem Informasi)</h4>
+                     </div>
+                  </div>
+               </div>
+   
+            <?php endfor; ?>
+
+         </div>
+      </div>
+   </section>
+
+<!--  <?php include 'footer.php'; ?> -->
