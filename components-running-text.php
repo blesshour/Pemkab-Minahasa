25ece1dce0cd9c0f53d5eed7d31020198fb1f041
BillyOroh <45728852+BillyOroh@users.noreply.github.com>
2018-12-12 18:16:37 +0800

    Add files via upload

---
+++ components-running-text.php
@@ -0,0 +1,51 @@
+<?php include 'header.php';?>
+
+<!-- RUNNING TEXT -->
+  <section id="running-text">
+   <div class="container-fluid">
+    <div class="row">
+      <div class="col-md-12 text-center">
+        
+        <div class="carousel slide" id="my-carousel" data-ride="carousel">
+           
+
+              <div class="carousel-inner-text">
+                <div class="carousel-item text-center carousel-text">
+                  <div class="img-box m-auto">
+                    
+                  </div>
+                  <h5 class="py-1 my-1">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</h5>
+                  
+                </div>
+                 <div class="carousel-item text-center carousel-text active">
+                  <div class="img-box m-auto">
+                    
+                  </div>
+                  <h5 class="py-1 my-1">Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</h5>
+                  
+                </div>
+                 <div class="carousel-item text-center carousel-text">
+                  <div class="img-box m-auto">
+                    
+                  </div>
+                  <h5 class="py-1 my-1">Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</h5>
+                  
+                </div>
+                <div class="carousel-item text-center carousel-text">
+                  <div class="img-box m-auto">
+                    
+                  </div>
+                  <h5 class="py-1 my-1">Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                  consequat. </h5>
+                  
+                </div>
+              </div>
+            
+        </div>
+      </div>
+    </div>
+  </div>
+  </section>
+    <!-- AKHIR RUNNING TEXT -->
+
+ <?php include 'footer.php';?>
