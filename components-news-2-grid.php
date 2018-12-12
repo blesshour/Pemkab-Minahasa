f981c2559b469eb9ddc1e3105723dcf7c2d13973
BillyOroh <45728852+BillyOroh@users.noreply.github.com>
2018-12-12 11:05:09 +0800

    Add files via upload

---
+++ components-news-2-grid.php
@@ -0,0 +1,88 @@
+ <?php include 'header.php';?>
+ 
+ <section id="news-2-grid">
+   <div class="container-fluid">
+    <div class="row">
+      <div class="col-md text-center kolom1">
+        <div class="carousel slide carousel-fade" id="my-carousel" data-ride="carousel">
+           
+
+              <div class="carousel-inner">
+                <div class="carousel-item text-center carousel1 active">
+                  
+                    <img src="" class="w-100 ">
+                  
+                  <h5>Gambar 1</h5>
+                  
+                </div>
+                 <div class="carousel-item text-center carousel2">
+                  
+                    <img src="" class="w-100 ">
+                  
+                  <h5>Gambar 2</h5>
+                  
+                </div>
+                 <div class="carousel-item text-center carousel3">
+                  
+                    <img src="" class="w-100 ">
+                  
+                  <h5>Gambar 3</h5>
+                  
+                </div>
+                <div class="carousel-item text-center carousel4">
+                  
+                    <img src="" class="w-100 ">
+                  
+                  <h5>Gambar 4</h5>
+                  
+                </div>
+              </div>
+              
+        </div>
+
+
+      </div>
+        <div class="col-md text-center">
+        <div class="carousel slide carousel-fade" id="my-carousel" data-ride="carousel">
+           
+
+              <div class="carousel-inner">
+                <div class="carousel-item text-center carousel4 active">
+                  
+                    <img src="" class="w-100 ">
+                  
+                  <h5>Gambar 1</h5>
+                  
+                </div>
+                 <div class="carousel-item text-center carousel3">
+                  
+                    <img src="" class="w-100 ">
+                  
+                  <h5>Gambar 2</h5>
+                  
+                </div>
+                 <div class="carousel-item text-center carousel2">
+                  
+                    <img src="" class="w-100 ">
+                  
+                  <h5>Gambar 3</h5>
+                  
+                </div>
+                <div class="carousel-item text-center carousel1">
+                  
+                    <img src="" class="w-100 ">
+                  
+                  <h5>Gambar 4</h5>
+                  
+                </div>
+              </div>
+              
+        </div>
+
+        
+      </div>
+    </div>
+  </div>
+  </section>
+
+  <?php include 'footer.php';?>
