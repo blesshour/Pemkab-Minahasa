bc5169078ea839b5d6d08547a0ddc406969019eb
pricelangela <45728920+pricelangela@users.noreply.github.com>
2018-12-13 17:22:56 +0800

    components-pemerintah.php

---
+++ components-pemenrintah-profil-wakilbupati.php
@@ -0,0 +1,190 @@
+<!-- Bootstrap CSS -->
+<!-- <link rel="stylesheet" href="inc/bootstrap/bootstrap.min.css">
+<link rel="stylesheet" href="css/main.css">
+<link href="inc/animate/animate.min.css" rel="stylesheet">
+<link href="inc/magnific-popup/magnific-popup.css" rel="stylesheet">  
+<link href="inc/bootstrap/bootstrap-4.1.3.min.css" rel="stylesheet">  
+<link href="inc/jquery/jquery-3.3.1.min.js">  
+<link href="inc/popper/popper-1.14.3.min.js">  
+<link href="inc/bootstrap/bootstrap-4.1.3.min.js">   -->
+
+
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+
+<div class="container">
+  <div class="jumbotron">
+    <h1>Wakil Bupati Minahasa</h1>      
+    <p>Profil Wakil Bupati Minahasa 2018-2023</p>
+  </div>  
+</div>
+
+<div class="container">
+    <div class="row">
+        <div class="col-md-4">
+            <div align="center">
+            <img src="img/logokabminahasa2.png" alt="">
+            </div>
+        </div>
+
+        <div class="col-md-8">
+            <div class="container">
+              <br>
+              <!-- Nav tabs -->
+              <ul class="nav nav-tabs" role="tablist">
+                <li class="nav-item">
+                  <a class="nav-link active" data-toggle="tab" href="#home">Data Pribadi</a>
+              </li>
+              <li class="nav-item">
+                  <a class="nav-link" data-toggle="tab" href="#menu1">Riwayat Pendidikan</a>
+              </li>
+              <li class="nav-item">
+                  <a class="nav-link" data-toggle="tab" href="#menu2">Jabatan Fungsional</a>
+              </li>
+              <li class="nav-item">
+                  <a class="nav-link" data-toggle="tab" href="#menu3">Pengalaman Organisasi</a>
+              </li>
+              <li class="nav-item">
+                  <a class="nav-link" data-toggle="tab" href="#menu4">Kepanitiaan</a>
+              </li>
+              <li class="nav-item">
+                  <a class="nav-link" data-toggle="tab" href="#menu5">Kunjungan ke Luar Negeri</a>
+              </li>
+              <li class="nav-item">
+                  <a class="nav-link" data-toggle="tab" href="#menu6">Penghargaan</a>
+              </li>
+              <li class="nav-item">
+                  <a class="nav-link" data-toggle="tab" href="#menu7">Data Keluarga</a>
+              </li>
+              <li class="nav-item">
+                  <a class="nav-link" data-toggle="tab" href="#menu8">Lain-lain</a>
+              </li>
+          </ul>
+
+          <!-- Tab panes -->
+          <div class="tab-content">
+            <div id="home" class="container tab-pane active"><br>
+                <table border="0px" align="left" width="90%">
+                <tr>
+                    <td>Nama Lengkap</td>
+                    <td>:</td>
+                    <td>Robby Dondokambey, S.Si</td>
+                </tr>
+                <tr>
+                    <td>TTL</td>
+                    <td>:</td>
+                    <td> </td>
+                </tr>
+                <tr>
+                    <td>NIK</td>
+                    <td>:</td>
+                    <td> </td>
+                </tr>
+                <tr>
+                    <td>Usia</td>
+                    <td>:</td>
+                    <td> </td>
+                </tr>
+                <tr>
+                    <td>Alamat</td>
+                    <td>:</td>
+                    <td> </td>
+                </tr>
+                <tr>
+                    <td>Email</td>
+                    <td>:</td>
+                    <td> </td>
+                </tr>
+                <tr>
+                    <td>Status Perkawinan</td>
+                    <td>:</td>
+                    <td>Kawin</td>
+                </tr>
+                <tr>
+                    <td>Jenis Kelamin</td>
+                    <td>:</td>
+                    <td>Laki-laki</td>
+                </tr>
+                <tr>
+                    <td>Agama</td>
+                    <td>:</td>
+                    <td>Kristen Protestan</td>
+                </tr>
+                <tr>
+                    <td>NPWP</td>
+                    <td>:</td>
+                    <td> </td>
+                </tr>
+                <tr>
+                    <td>Hobi</td>
+                    <td>:</td>
+                    <td> </td>
+                </tr> 
+                <tr height="50px">
+                    <td> </td>
+                    <td> </td>
+                    <td><b><i> </i></b></td>
+                </tr>
+                </table>
+          </div>
+
+          <div id="menu1" class="container tab-pane fade"><br>
+              <h4>PENDIDIKAN FORMAL</h4>
+              SD Negeri IX Manado <br>
+              SMP Negeri II Manado <br>
+              SMA Negeri 1 Manado <br>
+              STPN Jogjakarta <br>
+              Universitas Gajah Mada Jogjakarta<br>
+              ISM Jakarta<br>
+              <p><p>
+              Birokrat terakhir Kepada Badan Pertahanan Minsel, Dirut PDAM Minahasa
+          </div>
+
+          <div id="menu2" class="container tab-pane fade"><br>
+
+          </div>
+
+          <div id="menu3" class="container tab-pane fade"><br>
+            <h4>ORGANISASI PROFESI</h4>
+            <ul>
+                <li>Anggota Perbankin Sulut
+                <li>Ketua Sasana Tinju Banteng Sulut</li>
+                <li>Wakil Ketua PDI Perjuangan Minahasa</li>
+                <li>Ketua Umum Ormas BMOD</li>
+            </ul>
+            <p><p>
+            <h4>ORGANISASI KEAGAMAAN</h4>
+            <ul>
+              <li>Pelayan Khusus 2 Periode</li>
+              <li>Bendahara Pemb BMIM Bukit Moria Tikbar</li>
+              <li>Bendahara Pemb PPWG Sinode GMIM Kaaten Tomohon (sampai sekarang)</li>
+              <li>Ketua Komisi Pemb GMIM Petra Sawangan Tombulu</li>
+              <li>Ketua Umum Panitia KPA Sinode GMIM Bumi Perkemahan Leilem</li>
+            </ul>
+          </div>
+
+          <div id="menu4" class="container tab-pane fade"><br>
+             
+          </div>
+
+          <div id="menu5" class="container tab-pane fade"><br>
+             
+          </div>
+
+          <div id="menu6" class="container tab-pane fade"><br>
+             
+          </div>
+
+          <div id="menu7" class="container tab-pane fade"><br> 
+
+          </div>
+
+          <div id="menu8" class="container tab-pane fade"><br>
+             
+          </div>
+  </div>
+</div>
+</div>
+</div>
