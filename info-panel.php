517ac1fc5a269ed9d5c09cb0882a746a648d01e2
BillyOroh <45728852+BillyOroh@users.noreply.github.com>
2018-12-23 19:17:52 +0800

    Create info-panel.php

---
+++ info-panel.php
@@ -0,0 +1,23 @@
+<!-- INFO PANEL -->
+    <div class="row justify-content-center">
+        <div class="col-10 info-panel">
+          <div class="row">
+            <div class="col-lg">
+              <img src="img/employee.png" alt="Employee" class="float-left">
+              <h4>Pesan Bahan Baku</h4>
+              <p>Lorem ipsum dolor sit amet.</p>
+            </div>
+            <div class="col-lg">
+              <img src="img/hires.png" alt="High Ress" class="float-left">
+              <h4>Pesan Makanan</h4>
+              <p>Lorem ipsum dolor sit amet.</p>
+            </div>
+            <div class="col-lg">
+              <img src="img/security.png" alt="Security" class="float-left">
+              <h4>Pesan Snack</h4>
+              <p>Lorem ipsum dolor sit amet.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    <!-- AKHIR INFO PANEL -->
